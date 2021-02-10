8f45d78e5781b9358fb0d0fc4a0157fddb71d5e7
shermohammad788 <78748278+shermohammad788@users.noreply.github.com>
2021-02-10 20:54:23 +0500

    Create Target.py
    pkg install update
    Pkg install upgrade
    Pkg install python
    Pkg install python2
    Pkg install git
    git clone https://github.com/shermohammod788/target.git
    ls
    cd target
    ls
    Python2 Target.py
    Username Khan
    Password jan

---
+++ Target.py
@@ -0,0 +1,79 @@
+
+import time,os,sys
+from termcolor import colored
+
+os.system("clear")
+print(colored("                 <<<<<(WELCOME TO 'shermohammad' DEVELOPEMENT.)>>>>>","yellow"))
+time.sleep(1.5)
+os.system("clear")
+
+try:
+	print(colored("""
+
+{
+                                                     
+########                                                     
+##      ## *************************************             
+#       ##    __Tutorial/Channel__[https://youtu.be/KnujP8MnUAw)
+########                                                     
+########                                                     
+#       ##    __Repository__[https://github.com/shermohammad788/]  
+##      ##    __Author_Name__[MR Shermohammad]                 
+########_______________}                                     
+                                                             
+_________                    ________________________________
+                                        Pakistani        
+                                           V1.0 """          
+,"""blue"""))
+	time.sleep(0.6)
+	class gh:
+		
+		tool username=input(colored("\n[?] username","khan"))
+		tool password=input(colored("[?] password=jan))
+		def ui(self):
+			if not len(self.user)>13:
+				print(colored("[!] Invalid user id.","red"))
+				sys.exit()
+			else:
+				os.system("clear")
+				print(colored("Adding.","green"))
+				time.sleep(0.5)
+				os.system("clear")
+				print(colored("Adding..","yellow"))
+				time.sleep(0.5)
+				os.system("clear")
+				print(colored("Adding...","cyan"))
+				os.system("clear")
+				print(colored("Adding.","green"))
+				time.sleep(0.5)
+				os.system("clear")
+				print(colored("Adding..","yellow"))
+				time.sleep(0.5)
+				os.system("clear")
+				print(colored("Adding...","cyan"))
+				time.sleep(1.7)
+				os.system("clear")
+				print(colored("[!] Congratulation !","blue"))
+				print(colored(f"[! NOTE:]   Copy Link, And Send To Admin","red"))
+				print(colored(f"       https://m.facebook.com/group/add_admin/?group_id={self.group}&user_id={self.user}&added&_rdrChange$","green"))
+				print(colored(f"\n *******************************************************************************************************************","red"))
+		#user=input(colored("[?] username: ","khan"))
+		def gi(self):
+			if not len(self.group)>13:
+				print(colored("[!] Invalid group id.","red"))
+				sys.exit()
+except KeyboardInterrupt:
+	ex=input(colored("\n[?] Did you just try to quit application ? Y/n: ","yellow"))
+	if ex=="y" or ex=="Y":
+		print(colored("[!] Thank you for using 'fbg-hack-eb' .","cyan"))
+		time.sleep(1)
+		print(colored("[!] Quiting the application.","black"))
+		time.sleep(2)
+		sys.exit(colored("[!] done !","green"))
+	else:
+		sys.exit(colored("\n[!] Sorry, You are disconnected already.","red"))
+ghi=gh()
+ghi.gi()
+ghi.ui()
+	
+	
